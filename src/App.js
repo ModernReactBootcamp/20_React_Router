30da49c96ab1b40f17a993a95ec58d26ac75464e
kdord <katya.koliada49@gmail.com>
2019-12-19 16:11:48 +0200

    add NavLinks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import logo from "./logo.svg";
+import { Route, Switch, NavLink } from "react-router-dom";
 import "./App.css";
 import About from "./About";
 import Contact from "./Contact";
 import Dog from "./Dog";
 
+const Hater = () => <h1>HATE DOGS</h1>;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/dog" component={Dog} />
+        <nav className="App-nav">
+          <NavLink exact activeClassName="active-link" to="/">
+            About
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/contact">
+            Contact
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/dog/c">
+            Dog(c)
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/dog/c">
+            Dog(r)
+          </NavLink>
+        </nav>
+        <Route exact path="/" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/dog/c" component={() => <Dog name="Muffins" />} />
+        <Route exact path="/dog/r" render={() => <Dog name="Biscuits" />} />
+        <Route exact path="/dog/hater" component={Hater} />
       </div>
     );
   }
--- src/Dog.js
+++ src/Dog.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class Dog extends Component {
+  componentDidMount() {
+    console.log("dog did mount");
+  }
+
+  componentWillUnmount() {
+    console.log("dog will unmount");
+  }
   render() {
+    console.log("dog render");
     return (
       <div className="Dog">
         <h1>DOG PAGE</h1>
+        <h3>This dog is named: {this.props.name}</h3>
         <img src="https://images-na.ssl-images-amazon.com/images/I/514e1-wkceL._SX331_BO1,204,203,200_.jpg" />
       </div>
     );
