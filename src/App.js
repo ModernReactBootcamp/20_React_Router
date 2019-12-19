e5e55323e37fac97561f1c8718ccc3142a519c1b
kdord <katya.koliada49@gmail.com>
2019-12-19 15:31:04 +0200

    add first route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import About from "./About";
@@ -6,31 +7,10 @@ import Contact from "./Contact";
 import Dog from "./Dog";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      page: "about"
-    };
-  }
-
-  changePage(newPage) {
-    this.setState({ page: newPage });
-  }
-  renderPage() {
-    if (this.state.page === "about") return <About />;
-    else if (this.state.page === "contact") return <Contact />;
-    else if (this.state.page === "dog") return <Dog />;
-  }
   render() {
     return (
       <div className="App">
-        <nav className="App-nav">
-          <a onClick={() => this.changePage("about")}>About</a>
-          <a onClick={() => this.changePage("contact")}>Contact</a>
-          <a onClick={() => this.changePage("dog")}>Dog</a>
-        </nav>
-        {this.renderPage()}
+        <Route path="/dog" component={Dog} />
       </div>
     );
   }
